c029fb04aaad7e82e0ac637ce62e4770c655ca8e
IXOFIXOF <IXOF@POCZTA.ONET.PL>
2018-05-22 23:36:11 +0200

    Init commit

---
+++ Mieszkania/Mieszkania.cpp
@@ -0,0 +1,13 @@
+// Mieszkania.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Klasy bazowe mieszkania, ludzie i klasy pochodne blok, dom, bliŸniak, w³aœciciel, lokator itd. Przeci¹¿yæ operator strumienia i dodawania i odejmowania z do mieszkania lokatorów i w³aœcicieli i parê wed³ug w³asnego pomys³u.\n";
+	std::cin.get();
+    return 0;
+}
+
